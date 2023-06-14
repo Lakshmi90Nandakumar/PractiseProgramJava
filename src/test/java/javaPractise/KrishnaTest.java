8d788a110a8e5fdea183d037b3ed2e889ed2fbca
Lakshmi <Lakshmi@DESKTOP-1ABT9ES>
2023-06-14 10:35:39 +0530

    added java programs

--- src/test/java/javaPractise/KrishnaTest.java
+++ src/test/java/javaPractise/KrishnaTest.java
@@ -1,5 +1,7 @@
 package javaPractise;
 
+import java.util.Arrays;
+
 import org.testng.reporters.jq.Main;
 
 public class KrishnaTest
@@ -36,6 +38,50 @@ public class KrishnaTest
 			}
 		}
 		System.out.println("From bubble sort: "+a[1]);
+		
+		String s="Lakshmi chitra is good",rev="",rev1="";
+		for(int i=0;i<s.length();i++)
+		{
+			if(s.charAt(i)!=' ')
+			{
+				rev=s.charAt(i)+rev;
+			}
+		}
+		System.out.println(rev);
+		
+		for(int i=0;i<s.length();i++)
+		{
+			if(s.charAt(i)==' ')
+				rev=rev.substring(0,i)+" "+rev.substring(i,rev.length());
+		}
+		System.out.println(rev);
+		
+		//anagram
+		
+		String st="woood",st1="doowk";boolean flag=true;int count=0;
+		char[] ch=st.toCharArray();Arrays.sort(ch);
+		char[] ch1=st1.toCharArray();Arrays.sort(ch1);
+		if(ch.length==ch1.length)
+		{
+			int j=0;
+			for(int i=0;i<ch.length;i++)
+			{
+				   
+				   if(ch[i]==ch1[j])
+	            	   count++;
+                 j++;
+				                
+			}
+			if(count==ch.length)
+				System.out.println("Anagram");
+			else
+				System.out.println("Not anagram");
+		}
+		else
+		{
+			System.out.println("Not anagram");
+		}
+		
     }
 
 }
--- src/test/java/javaPractise/PrintTrdEleInSeries.java
+++ src/test/java/javaPractise/PrintTrdEleInSeries.java
@@ -6,7 +6,7 @@ public class PrintTrdEleInSeries {
 
 	public static void main(String[] args) {
 
-		Scanner sc=new Scanner(System.in);
+	/*	Scanner sc=new Scanner(System.in);
 		System.out.println("Enter the Number");
 		long input=sc.nextLong();
 		String s=Long.toString(input);	
@@ -19,7 +19,29 @@ public class PrintTrdEleInSeries {
 				System.out.print(ch[i]+" ");
 			}
 		}
+		*/
+		int[] a= {8,8,8,7,4,6,8,7,2,1,5,6,7,8};
+		int fmax=a[0],smax=a[4],tmax=a[5];
 		
+		for(int i=1;i<a.length;i++)
+		{
+			if(a[i]>fmax)
+			{
+				tmax=smax;
+				smax=fmax;
+				fmax=a[i];
+			}
+			else if((a[i]!=fmax) && (a[i]>smax))
+			{
+				tmax=smax;
+				smax=a[i];
+			}
+			else if(a[i]!=smax && a[i]!=fmax && a[i]>tmax)
+			{
+				tmax=a[i];
+			}
+		}
+		System.out.println(fmax+" "+smax+" "+tmax);
 	}
 
 }
